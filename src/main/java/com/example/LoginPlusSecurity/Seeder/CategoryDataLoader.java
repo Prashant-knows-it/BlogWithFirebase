a7226d5c81593ab933627d6c9bc0d80169fc4874
Prashant Kumar <133897555+SpidermanCodes@users.noreply.github.com>
2025-01-04 21:08:58 +0530

    category list redefined

--- src/main/java/com/example/LoginPlusSecurity/Seeder/CategoryDataLoader.java
+++ src/main/java/com/example/LoginPlusSecurity/Seeder/CategoryDataLoader.java
@@ -19,15 +19,28 @@ public class CategoryDataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
         if (categoryRepository.count() == 0) {
             categoryRepository.save(new Category("Technology"));
-            categoryRepository.save(new Category("Mindfulness"));
-            categoryRepository.save(new Category("News"));
-            categoryRepository.save(new Category("Education"));
+            categoryRepository.save(new Category("Politics"));
+
             categoryRepository.save(new Category("Health"));
-            categoryRepository.save(new Category("Travel"));
-            categoryRepository.save(new Category("Lifestyle"));
+            categoryRepository.save(new Category("Mental Health"));
+            categoryRepository.save(new Category("Self-Help"));
+            categoryRepository.save(new Category("Psychology"));
+            categoryRepository.save(new Category("Life Lessons"));
+
+            categoryRepository.save(new Category("Relationships"));
+            categoryRepository.save(new Category("Parenting"));
+            categoryRepository.save(new Category("For Male"));
+            categoryRepository.save(new Category("For Female"));
+
+            categoryRepository.save(new Category("How-To"));
+            categoryRepository.save(new Category("Things-To-Do"));
+            categoryRepository.save(new Category("Tactics"));
+
+            categoryRepository.save(new Category("Book Review"));
+            categoryRepository.save(new Category("Feelings"));
             categoryRepository.save(new Category("Adult"));
-            categoryRepository.save(new Category("Entertainment"));
-            categoryRepository.save(new Category("Sports"));
+            categoryRepository.save(new Category("Other"));
+
         }
     }
 }
